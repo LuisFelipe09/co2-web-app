fabb25582db9d621172a1674e525f9da56096843
onedrako <onedrako@gmail.com>
2022-09-04 11:38:01 -0500

    Add Eslint config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import CreateProject from "./views/CreateProject";
-import Home from "./views/home";
-import ProjectList from "./views/ProjectsList";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import CreateProject from './views/CreateProject'
+import Home from './views/home'
+import ProjectList from './views/ProjectsList'
 
-function App() {
+function App () {
   return (
     <Routes>
       <Route path="/" exact element={<Home/>} />
       <Route path="/projects" exact element={<ProjectList/>} />
       <Route path="/create-project" exact element={<CreateProject/>} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from './NavBar'
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <>
       <NavBar/>
