f432c9ee53e25f3912c89b50c79ebe680f72f1fb
onedrako <onedrako@gmail.com>
2022-09-04 19:39:35 -0500

    Add styles to how to participate section

--- src/components/Home/Participate.jsx
+++ src/components/Home/Participate.jsx
@@ -8,40 +8,44 @@ const Participate = () => {
 
       <div className='how'>
 
-        <div className='how__who'>
-          <h3 className='how_subtitle'>Who?</h3>
-          <p>Any person or organization that wants to contribute to the fight against climate change.</p>
-          <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/2821/2821858.png" alt="" />
-            <p>Organization</p>
+        <div className='how__container'>
+          <h3 className='how__subtitle'>Who?</h3>
+          <p className='how__text'>Any person or organization that wants to contribute to the fight against climate change.</p>
+
+          <div className='how-image-text--doble'>
+            <div>
+              <img className='how__image' src="https://cdn-icons-png.flaticon.com/512/2821/2821858.png" alt="" />
+              <p className='how__text'>Organization</p>
+            </div>
+            <div>
+              <img className='how__image' src="https://cdn-icons-png.flaticon.com/512/1256/1256650.png" alt="" />
+              <p className='how__text'>Individuals</p>
+            </div>
           </div>
-          <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/1256/1256650.png" alt="" />
-            <p>Individuals</p>
+
+        </div>
+
+        <div className='how__container'>
+          <h3 className='how__subtitle'>Choose</h3>
+          <div className='how__image-text'>
+            <p className='how__text'>Choose the projects and choose the one that best suits with your cause.</p>
+            <img className='how__image' src="https://cdn-icons-png.flaticon.com/512/2230/2230606.png" alt="" />
           </div>
         </div>
 
-        <div className='how_container'>
-          <h3 className='how_subtitle'>Choose</h3>
-          <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/2230/2230606.png" alt="" />
-            <p>Choose the projects and choose the one that best suits with your cause.</p>
+        <div className='how__container'>
+          <h3 className='how__subtitle'>How it works?</h3>
+          <div className='how__image-text'>
+            <p className='how__text'>Using blockchain technology by the Etherum Virtual machine, the contract process your transaction sending your purchase to the project account.</p>
+            <img className='how__image' src="https://cdn-icons-png.flaticon.com/512/5960/5960252.png" alt="" />
           </div>
         </div>
 
-        <div className='how_container'>
-          <h3 className='how_subtitle'>How it works?</h3>
-          <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/5960/5960252.png" alt="" />
-            <p>Using blockchain technology by the Etherum Virtual machine, the contract process your transaction sending your purchase to the project account</p>
-          </div>
-        </div>
-
-        <div className='how_container'>
-          <h3 className='how_subtitle'>What do I get at the end?</h3>
-          <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/6298/6298900.png" alt="" />
-            <p>Your purchase gives you an NFT that represents a Green Bond of the project, your NFT has an unique ID and transaction that will be associate to you wallet</p>
+        <div className='how__container'>
+          <h3 className='how__subtitle'>What do I get at the end?</h3>
+          <div className='how__image-text'>
+            <p className='how__text'>Your purchase gives you an NFT that represents a Green Bond of the project, your NFT has an unique ID and transaction that will be associate to you wallet.</p>
+            <img className='how__image' src="https://cdn-icons-png.flaticon.com/512/6298/6298900.png" alt="" />
           </div>
         </div>
 
--- src/styles/Home/Participate.js
+++ src/styles/Home/Participate.js
@@ -10,6 +10,51 @@ export const ParticipateStyled = styled.section`
   
   & > h2{
     font-size: 2.5rem;
-    margin-bottom: 30px;
+    margin-bottom: 45px;
+  }
+  
+  & > .how{
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    width: 90%;
+    gap: 45px;
+
+    & > .how__container{
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+
+      & > .how__subtitle{
+        font-size: 1.5rem;
+        text-align: center;
+      }
+
+      & > .how__image-text{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+      }
+
+      & > .how-image-text--doble{
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        gap: 10px;
+
+
+      }
+
+      & .how__image{
+        width: 150px;
+        margin: 0 auto;
+      }
+
+      & .how__text{
+        margin-top: 20px;
+        font-size: 1.2rem;
+        text-align: justify;
+      }
+    }
   }
 `
